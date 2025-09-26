254f210988ad821b7d4aa49df98aec22f659978d
Yael Ramírez <yaelramirezmendez@gmail.com>
2025-09-25 20:38:11 -0600

    Foundation setup.

---
+++ user/src/main/java/com/yrmz/chirp/MyClass.kt
@@ -0,0 +1,4 @@
+package com.yrmz.chirp
+
+class MyClass {
+}
---
+++ user/src/main/java/com/yrmz/chirp/domain/model/AuthenticatedUser.kt
@@ -0,0 +1,7 @@
+package com.yrmz.chirp.domain.model
+
+data class AuthenticatedUser(
+    val user: User,
+    val accessToken: String,
+    val refreshToken: String
+)
