ff1e7d6cf43e8354d1a233e5e1049663743b1290
yaelrmz <yaelramirezmendez@gmail.com>
2025-11-12 22:03:20 -0600

    Firebase Admin SDk setup.

---
+++ notification/src/main/java/com/yrmz/chirp/domain/model/PushNotification.kt
@@ -0,0 +1,13 @@
+package com.yrmz.chirp.domain.model
+
+import com.yrmz.chirp.domain.type.ChatId
+import java.util.UUID
+
+data class PushNotification(
+    val id: String = UUID.randomUUID().toString(),
+    val title: String,
+    val recipients: List<DeviceToken>,
+    val message: String,
+    val chatId: ChatId,
+    val data: Map<String, String>
+)
